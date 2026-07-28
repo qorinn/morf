a79dc1db9e97311fc45e935c9b828521721748b4
Paládi Bálint <paladi.balint14@gmail.com>
2026-07-28 15:33:21 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://morfkit.com',
   vite: {
     plugins: [tailwindcss()],
     resolve: {
@@ -29,5 +31,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
